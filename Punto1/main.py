72501910829d6c64dbc23660a8230ca89e288ff7
criscujia <cjcujia@uniguajira.edu.co>
2020-09-15 23:52:39 -0500

    punto 1 modulado

--- Punto1/main.py
+++ Punto1/main.py
@@ -1,19 +1,4 @@
-
-class Alumno:
-
-    def __init__(self):
-        self.nombre=input("Ingrese el nombre del alumno >>")
-        self.nota=float(input(f"Ingrese la nota del {self.nombre} >>"))
-
-    def mostrar(self):
-        print(f"El nombre del alumno es:  {self.nombre} ")
-        print(f"La nota del alumno es:  {self.nota} ")
-    
-    def mensaje(self):
-        if self.nota>=3:
-            print(f"El alumno {self.nombre} aprobo con una nota de {self.nota}")
-        else:
-            print(f"El alumno {self.nombre} reprobo con una nota de {self.nota}")
+from library.Clase import*
 
 def main():
     Alumno1=Alumno()
